e0cf111b2dd40f991c80969295c3656d9ec0f784
adrianboh@gmail.com <adrianboh@gmail.com>
2023-04-17 17:00:54 +0200

    errore

--- la-mia-pizzeria-crud-mvc/Program.cs
+++ la-mia-pizzeria-crud-mvc/Program.cs
@@ -1,43 +1,39 @@
+
+
 using la_mia_pizzeria_static.Models;
 
-namespace la_mia_pizzeria_crud_mvc
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    internal class Program
-    {
-        private static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Pizza}/{action=Index}/{id?}");
-
-            using (var ctx = new PizzeriaContext())
-            {
-                ctx.seed();
-            }
-
-            app.Run();
-        }
-    }
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Pizza}/{action=Index}/{id?}");
+
+using (var ctx = new PizzeriaContext())
+{
+    ctx.seed();
+}
+
+app.Run();
